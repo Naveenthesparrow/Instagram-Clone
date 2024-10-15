390e8e8503514bcee6d60154cfd4e5d1ad1c101c
Naveen <naveenneevan6380@gmail.com>
2024-10-15 14:21:46 +0530

    done

--- Instagram-Clone/Frontend/Instagram_Clone/src/Pages/AppLayout.jsx
+++ Instagram-Clone/Frontend/Instagram_Clone/src/Pages/AppLayout.jsx
@@ -34,9 +34,12 @@ const AppLayout = () => {
   return (
     <div className="home w-full min-h-screen">
       <div className="w-full h-auto flex items-start justify-between md:gap-x-16 sm:gap-x-8 gap-x-4 relative">
-        {/* sidebar  */}
+        {/* side*/}
         <div className="lg:w-[18%] sm:w-none md:w-none h-[100vh] pt-10 px-3 border-r border-r-gray-500 sticky top-0 left-0 lg:block sm:hidden md:block hidden">
-          <LargeNav openLogoutModal={openLogoutModal} openModal={openModal}></LargeNav>
+          <LargeNav
+            openLogoutModal={openLogoutModal}
+            openModal={openModal}
+          ></LargeNav>
         </div>
         <div className="w-full h-auto py-1 px-3 border-t border-t-[#1d1d1d] fixed bottom-0 left-0 lg:hidden md:hidden sm:block z-50 bg-white">
           <MobileNav openModal={openModal}></MobileNav>
@@ -46,7 +49,9 @@ const AppLayout = () => {
         </div>
         <Outlet context={{ newPost, updateNewPost }}></Outlet>
       </div>
-      {isModalOpen && <CreatePost closeModal={closeModal} updateNewPost={updateNewPost} />}
+      {isModalOpen && (
+        <CreatePost closeModal={closeModal} updateNewPost={updateNewPost} />
+      )}
 
       {isLogoutModalOpen && (
         <Logout onClose={closeLogoutModal} onConfirm={handleLogout} />
